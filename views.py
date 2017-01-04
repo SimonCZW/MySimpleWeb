41942fe86cc0364dd31d5d594ef59f28bbfef4cb
陈卓文 <chenzhuowen.simon@foxmail.com>
2017-01-04 15:20:09 +0800

    各班平均分需求按班级和科目单独返回总分,平均分,人数

--- views.py
+++ views.py
@@ -36,6 +36,58 @@ def fail_count():
             fails.append(data)
     return jsonify(fails)
 
+@app.route('/api/sclass')
+def getsclass():
+    """
+    统计各班总分和平均分,并按平均分排序
+    {
+  "auto1_eng": {
+    "count": 4,
+    "total_garde": 320
+  },
+  "auto2_en": {
+    "count": 1,
+    "total_garde": 70
+  },
+  "auto2_eng": {
+    "count": 1,
+    "total_garde": 70
+  }
+}
+    """
+    datas = Employ.find_all()
+    sclass = {}
+    count = 1
+    sclass_type = []
+    for data in datas:
+        #初始化数据
+        identify=str(data['sclass'])+'_'+str(data['cname'])
+        if identify not in sclass_type :
+            sclass_type.append(identify)
+            #sclass[data['sclass']][data['cname']]={'total_garde':int(data['garde']), 'count':count}
+            #sclass[str(data['sclass'])+str(data['cname'])] = {'total_garde':int(data['garde']), 'count':count}
+            sclass[identify] = {'total_garde':int(data['garde']), 'count':count}
+        else:
+            #sclass[data['sclass']][data['cname']]['total_garde'] += int(data['garde'])
+            #sclass[data['sclass']][data['cname']]['count'] += 1
+            #sclass[str(data['sclass']+str(data['cname'])]['total_garde'] += int(data['garde'])
+            #sclass[str(data['sclass']+str(data['cname'])]['count'] += 1
+            sclass[identify]['total_garde'] += int(data['garde'])
+            sclass[identify]['count'] += 1
+    class_total_garde = {}
+    for class_course,value in sclass.iteritems():
+        #班级每科的平均分
+        sclass[class_course]['avg_garde'] =  value['total_garde']/value['count']
+        #获取班级和课程名
+#        sclass_name, course = class_course.split('_')
+#        if sclass_name not in class_total_garde:
+#            class_total_garde[sclass_name]={}
+#            class_total_garde[sclass_name]['total_garde'] = 0
+#        else:
+#            class_total_garde[sclass_name]['total_garde'] += sclass[class_course]['avg_garde']
+#    return jsonify(class_total_garde)
+    return jsonify(sclass)
+
 @app.route('/api/honors')
 def honor_count():
     """
